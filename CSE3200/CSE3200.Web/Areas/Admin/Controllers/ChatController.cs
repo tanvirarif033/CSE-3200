5dc86ae9b980e4018582c874722c43d95919236c
eushaahmed08 <eushaahmed08@gmail.com>
2025-09-19 21:10:54 +0600

    Created  Admin Chat Controller and View

---
+++ CSE3200/CSE3200.Web/Areas/Admin/Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CSE3200.Infrastructure;
+using System.Security.Claims;
+
+namespace CSE3200.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ChatController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChatController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
